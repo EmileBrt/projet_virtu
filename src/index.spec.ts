8c927dd11864ee444373671cd7a0b3301700555d
EmileBrt <emile.berteloot@ensta-bretagne.org>
2022-11-29 09:23:40 +0100

    tkt ça marche

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -5,28 +5,32 @@ let server;
 
 describe('Server sysinfo available', () => {
   beforeAll(() => {
-    console.log(1);
     server = new Server();
-    console.log(2);
     server.createServer();
-    console.log(3);
     server.startServer();
-    console.log(4);
   })
 
   afterAll(() => {
     server.closeServer();
-    console.log("Server Closed\n");
   })
 
   it('should create server on port 8007', async () => {
-    const response = http.get('http://localhost:8007/api/v1/sysinfo', (response: http.IncomingMessage) => {
+    var options = {
+      host: 'http://127.0.0.1:8007',
+      path: '/api/v1/sysinfo/'
+    };
+    const response = http.get(options, (response: http.IncomingMessage) => {
+      
       expect(response.statusCode).toEqual(200);
     });
   });
 
   it('should not responde', async () => {
-    const response = http.get('http://localhost:8007/api/v1/ouiioi', (response: http.IncomingMessage) => {
+    var options = {
+      host: 'http://127.0.0.1:8007',
+      path: '/api/v1/ouiioi/'
+    };
+    const response = http.get(options, (response: http.IncomingMessage) => {
       expect(response.statusCode).toEqual(404);
     });
   });
